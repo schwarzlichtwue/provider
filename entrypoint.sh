ab1c42e5f16a3e3702558b5b47bd65c3c0c68cc2
schwarzlicht-wue <schwarzlicht.wuerzburg@gmail.com>
2020-02-14 14:07:17 +0100

    dynamic sftp syncing

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ tor &
 
 content-provider -e /app/.env -d /app/db/db.sqlite3 -u 12 \
 		--jekyll-source /source_repo --jekyll-target /target_repo \
-		-a www3.systemli.org -c /app/ssh_config -b /app/sftp-commands.txt \
+		-a www3.systemli.org -c /app/ssh_config -r /www/ \
